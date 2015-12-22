e57397495d038baa0f069898e4d0abb127552d39
Jan Lippert <ljan@mail.upb.de>
2015-12-22 19:36:11 +0100

    Translated some strings

--- app/models/old_folder_instance.rb
+++ app/models/old_folder_instance.rb
@@ -1,8 +1,8 @@
 class OldFolderInstance < ActiveRecord::Base
   belongs_to :old_folder
 
-  validates :number,    presence: true, :inclusion => {:in => 1..9}
+  validates :number,        presence: true, :inclusion => {:in => 1..9}
   validates :old_folder_id, presence: true
-  validates :barcodeId, presence: true
+  validates :barcodeId,     presence: true, uniqueness: true
 
 end
