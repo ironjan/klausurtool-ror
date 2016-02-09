47f40b6d2a10600790f61c44a7c4e11272203514
Jan Lippert <ljan@mail.upb.de>
2016-02-09 18:32:09 +0100

    Started work on #64

--- spec/models/old_folder_spec.rb
+++ spec/models/old_folder_spec.rb
@@ -16,52 +16,68 @@ describe OldFolder do
     @o2.destroy!
   end
 
-  it "has a valid factory" do
-    expect(FactoryGirl.build(:old_folder)).to be_valid
+  describe "OldFolder validations" do
+    it "has a valid factory" do
+      expect(FactoryGirl.build(:old_folder)).to be_valid
+    end
+
+    it "is not valid without title" do
+      expect(FactoryGirl.build(:old_folder, title: nil)).to_not be_valid
+    end
+
+    it "is not valid without contentType" do
+      expect(FactoryGirl.build(:old_folder, contentType: nil)).to_not be_valid
+    end
+
+    it "is not valid without color" do
+      expect(FactoryGirl.build(:old_folder, color: nil)).to_not be_valid
+    end
+
+    it "is not valid without area" do
+      expect(FactoryGirl.build(:old_folder, area: nil)).to_not be_valid
+    end
+
+    it "is not valid with a contentType that's not allowed" do
+      expect(FactoryGirl.build(:old_folder, contentType: "Wrong Type")).to_not be_valid
+    end
+
+    it "is not valid with an area that's not allowed" do
+      expect(FactoryGirl.build(:old_folder, area: "No Area")).to_not be_valid
+    end
   end
 
-  it "is not valid without title" do
-    expect(FactoryGirl.build(:old_folder, title: nil)).to_not be_valid
+  describe "Basic OldFolder Searches" do
+
+    it "returns everything for empty search" do
+      expect(OldFolder.search('')).to match_array([@o1, @o2])
+    end
+
+    it "returns no results for search matching nothing" do
+      expect(OldFolder.search("Title 3")).to match_array([])
+    end
+
+    it "returns 1 result for search matching exactly one folder" do
+      expect(OldFolder.search("Title 1")).to match_array([@o1])
+    end
+
+    it "returns 2 results for search matching 2 folders" do
+      expect(OldFolder.search("Title")).to match_array([@o1, @o2])
+    end
+
+    it "returns the correct result when searching for something with spaces" do
+      expect(OldFolder.search("Tit le 2")).to match_array([@o2])
+    end
   end
 
-  it "is not valid without contentType" do
-    expect(FactoryGirl.build(:old_folder, contentType: nil)).to_not be_valid
-  end
+  describe "OldFolder Search with Umlauts" do
+    it "finds Übersetzer when searching über" do
+      @o1 = FactoryGirl.build(:old_folder, title: "Übersetzer")
+      @o1.save!
 
-  it "is not valid without color" do
-    expect(FactoryGirl.build(:old_folder, color: nil)).to_not be_valid
-  end
+      expect(OldFolder.search("über")).to match_array([@o1])
 
-  it "is not valid without area" do
-    expect(FactoryGirl.build(:old_folder, area: nil)).to_not be_valid
-  end
-
-  it "is not valid with a contentType that's not allowed" do
-    expect(FactoryGirl.build(:old_folder, contentType: "Wrong Type")).to_not be_valid
-  end
-
-  it "is not valid with an area that's not allowed" do
-    expect(FactoryGirl.build(:old_folder, area: "No Area")).to_not be_valid
-  end
-
-  it "returns everything for empty search" do
-    expect(OldFolder.search('')).to match_array([@o1, @o2])
-  end
-
-  it "returns no results for search matching nothing" do
-    expect(OldFolder.search("Title 3")).to match_array([])
-  end
-
-  it "returns 1 result for search matching exactly one folder" do
-    expect(OldFolder.search("Title 1")).to match_array([@o1])
-  end
-
-  it "returns 2 results for search matching 2 folders" do
-    expect(OldFolder.search("Title")).to match_array([@o1, @o2])
-  end
-
-  it "returns the correct result when searching for something with spaces" do
-    expect(OldFolder.search("Tit le 2")).to match_array([@o2])
+      @o1.destroy!
+    end
   end
 
 end
