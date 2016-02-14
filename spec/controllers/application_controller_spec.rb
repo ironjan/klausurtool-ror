f38674b13981ae3f5f62a7461f092e0304a479f6
Jan Lippert <lippertsjan@gmail.com>
2016-02-14 16:32:14 +0100

    Added spec-templates for controllers

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe ApplicationController do
+	it "reminds me to implement spec for this controller"
+	it "returns the index when required"
+end
---
+++ spec/controllers/old_folders_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+describe OldFoldersController do
+	it "reminds me to implement spec for this controller"
+end
