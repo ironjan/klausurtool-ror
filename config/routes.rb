5598b943086fb7f72959735cb0cb1a1075482ed1
Jan Lippert <lippertsjan@gmail.com>
2016-03-20 19:51:35 +0100

    Scoped /ausleihe routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,24 @@ Rails.application.routes.draw do
   post 'feedback' => 'feedback#send_feedback', as: 'send_feedback'
 
 
-  # Navigateable "Ausleihe"-routes
-  get 'ausleihe' => 'ausleihe#index', as: 'ausleihe'
-  get 'ausleihe/lent' => 'ausleihe#lent', as: 'ausleihe_lent'
-  get 'ausleihe/history' => 'ausleihe#history', as: 'ausleihe_history'
-  get 'ausleihe/folders' => 'ausleihe#folders', as: 'ausleihe_folders'
-  get 'ausleihe/folders/:id' => 'ausleihe#folder_details', as: 'ausleihe_folder'
-  get 'ausleihe/exams' => 'ausleihe#exams', as: 'ausleihe_exams'
+  scope '/ausleihe' do
 
-  # Routes that should not be manually navigated to in "Ausleihe"
-  post 'ausleihe/switch' => 'ausleihe#switch', as: 'ausleihe_switch'
-  get  'ausleihe/lending_form' => 'ausleihe#lending_form', as: 'lending_form'
-  post 'ausleihe/lending_action' => 'ausleihe#lending_action', as: 'lending_action'
-  get  'ausleihe/returning_form' => 'ausleihe#returning_form', as: 'returning_form'
-  post 'ausleihe/returning_action' => 'ausleihe#returning_action', as: 'returning_action'
+    # Navigateable "Ausleihe"-routes
+    get '/' => 'ausleihe#index', as: 'ausleihe'
+    get '/lent' => 'ausleihe#lent', as: 'ausleihe_lent'
+    get '/history' => 'ausleihe#history', as: 'ausleihe_history'
+    get '/folders' => 'ausleihe#folders', as: 'ausleihe_folders'
+    get '/folders/:id' => 'ausleihe#folder_details', as: 'ausleihe_folder'
+    get '/exams' => 'ausleihe#exams', as: 'ausleihe_exams'
+
+    # Routes that should not be manually navigated to in "Ausleihe"
+    post '/switch' => 'ausleihe#switch', as: 'ausleihe_switch'
+    get '/lending_form' => 'ausleihe#lending_form', as: 'lending_form'
+    post '/lending_action' => 'ausleihe#lending_action', as: 'lending_action'
+    get '/returning_form' => 'ausleihe#returning_form', as: 'returning_form'
+    post '/returning_action' => 'ausleihe#returning_action', as: 'returning_action'
+
+  end
 
   scope '/internal/admin' do
     get '/' => 'old_folders#index'
@@ -39,66 +43,66 @@ Rails.application.routes.draw do
 
     namespace :admin do
 
-      get 'lent' => 'lendouts#lent'#, as: 'admin_lent'
-      get 'history' => 'lendouts#history'#, as: 'admin_history'
+      get 'lent' => 'lendouts#lent' #, as: 'admin_lent'
+      get 'history' => 'lendouts#history' #, as: 'admin_history'
 
 
     end
 
   end
 
-  # Current redirection so that everyone has time to update bookmarks
-  get '/admin/*path', to: redirect{ |params, request| "/internal/admin/#{params[:path]}?#{request.query_string}" }
+# Current redirection so that everyone has time to update bookmarks
+  get '/admin/*path', to: redirect { |params, request| "/internal/admin/#{params[:path]}?#{request.query_string}" }
 
   root 'application#index'
 
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+# Example of regular route:
+#   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+# Example of named route that can be invoked with purchase_url(id: product.id)
+#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+# Example resource route (maps HTTP verbs to controller actions automatically):
+#   resources :products
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+# Example resource route with options:
+#   resources :products do
+#     member do
+#       get 'short'
+#       post 'toggle'
+#     end
+#
+#     collection do
+#       get 'sold'
+#     end
+#   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+# Example resource route with sub-resources:
+#   resources :products do
+#     resources :comments, :sales
+#     resource :seller
+#   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+# Example resource route with more complex sub-resources:
+#   resources :products do
+#     resources :comments
+#     resources :sales do
+#       get 'recent', on: :collection
+#     end
+#   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+# Example resource route with concerns:
+#   concern :toggleable do
+#     post 'toggle'
+#   end
+#   resources :posts, concerns: :toggleable
+#   resources :photos, concerns: :toggleable
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+# Example resource route within a namespace:
+#   namespace :admin do
+#     # Directs /admin/products/* to Admin::ProductsController
+#     # (app/controllers/admin/products_controller.rb)
+#     resources :products
+#   end
 end
