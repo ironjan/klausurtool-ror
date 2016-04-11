f4a4bca26121d6091446d4a4d48c20b1d9dbe4c7
Jan Lippert <lippertsjan@gmail.com>
2016-04-11 22:12:18 +0200

    Added controller for covers

---
+++ app/controllers/internal/admin/print_controller.rb
@@ -0,0 +1,36 @@
+require 'barby'
+require 'barby/barcode/ean_8'
+require 'barby/outputter/html_outputter'
+
+module Internal
+  module Admin
+    class PrintController < ApplicationController
+      layout 'print'
+
+      def cover
+        barcode = params[:barcode]
+        if barcode.nil?
+          flash[:alert] = 'Kein Ordner-Exemplar angegeben.' and return
+        end
+
+        @old_folder_instance = OldFolderInstance.find_by(barcodeId: barcode)
+
+        if @old_folder_instance.nil?
+          flash[:alert] = "Ordner-Exemplar mit Barcode `#{barcode}` nicht gefunden."
+          @old_folder_instance = OldFolderInstance.new
+        end
+
+        Rails.logger.warn(barcode)
+        Rails.logger.warn("#{@old_folder_instance.inspect}")
+
+        padded_barcode = @old_folder_instance.barcodeId.to_s.rjust(7, '0')
+        Rails.logger.warn(padded_barcode)
+
+        barcode = Barby::EAN8.new(padded_barcode)
+
+        @barcode_for_html = Barby::HtmlOutputter.new(barcode)
+        @barcode_as_string = barcode.to_s
+      end
+    end
+  end
+end
