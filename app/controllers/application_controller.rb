5ef6dc9b2dd7ab5b541f02983274be31c4cf1457
Jan Lippert <lippertsjan@gmail.com>
2016-04-24 20:40:44 +0200

    Fixed #94: Added search for students

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
+  include PaginatedExamsList
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def index
+  	paginated_exams_list
   end
 end
