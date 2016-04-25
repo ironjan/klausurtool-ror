035520e47a4b227daa7bd604547805d7449883b4
Jan Lippert <lippertsjan@gmail.com>
2016-04-25 15:23:31 +0200

    Adapted spec for TOC with only unarchived

--- spec/views/old_folders/toc.html.erb_spec.rb
+++ spec/views/old_folders/toc.html.erb_spec.rb
@@ -4,7 +4,7 @@ describe "old_folders/toc.html.erb" do
   it "displays the " do
     old_folder = FactoryGirl.build(:old_folder, title: "My Name")
     assign(:old_folder, old_folder)
-
+    assign(:unarchived_exams, [])
     render
 
     expect(rendered).to include('Ordner: My Name')
