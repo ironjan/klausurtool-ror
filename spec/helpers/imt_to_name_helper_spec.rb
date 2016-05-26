ba9e301929f3c47a48e4d7408b13a9a790056510
Jan Lippert <lippertsjan@gmail.com>
2016-05-26 14:54:20 +0200

    Refactored imt_to_name_helper.rb

--- spec/helpers/imt_to_name_helper_spec.rb
+++ spec/helpers/imt_to_name_helper_spec.rb
@@ -15,37 +15,37 @@ describe ImtToNameHelper do
   describe "#name_for_login" do
     it "returns 'Keine Verbindung zum LDAP' when there's no LDAP connection" do
       ldap = double("Net::LDAP", :bind => false)
-      expect(name_for_login('', ldap)).to eq('Keine Verbindung zum LDAP')
+      expect(name_for_login('', ldap)).to eq('Keine Verbindung zum LDAP.')
     end
 
     it "returns 'Es konnte kein eindeutiger Name gefunden werden.' when there's no match" do
       ldap = double("Net::LDAP", :bind => true, :search => [])
       imt_login = 'no_match'
-      expect(name_for_login(imt_login, ldap)).to eq('Es konnte kein eindeutiger Name gefunden werden.')
+      expect(name_for_login(imt_login, ldap)).to eq('Fehler bei der LDAP-Suche.')
     end
 
 
-    it "returns 'Es konnte kein eindeutiger Name gefunden werden.' if there is more than one match" do
+    it "returns 'Fehler bei der LDAP-Suche.' if there is more than one match" do
       # This should not happen because every person has a unique login...
       mock_result = [double('Net::LDAP::Entry', :gecos => ['Real Name']), double('Net::LDAP::Entry', :gecos => ['Second Name'])]
       ldap = double('Net::LDAP', :bind => true, :search => mock_result)
-      expect(name_for_login('one match', ldap)).to eq('Es konnte kein eindeutiger Name gefunden werden.')
+      expect(name_for_login('one match', ldap)).to eq('Fehler bei der LDAP-Suche.')
 
     end
 
-    it "returns 'Keine Verbindung zum LDAP' if there is a NoBindResultError" do
+    it "returns 'Keine Verbindung zum LDAP.' if there is a NoBindResultError" do
       ldap = double(:bind => true)
       allow(ldap).to receive(:search).and_raise(Errno::ECONNREFUSED)
 
-      expect(name_for_login('', ldap)).to eq('Keine Verbindung zum LDAP')
+      expect(name_for_login('', ldap)).to eq('Keine Verbindung zum LDAP.')
 
     end
 
-    it 'returns "Unbekannter LDAP Fehler" if there is any another exception' do
+    it 'returns "Fehler bei der LDAP-Suche." if there is any another exception' do
       ldap = double(:bind => true)
       allow(ldap).to receive(:search).and_raise('AnotherError')
 
-      expect(name_for_login('', ldap)).to eq('Unbekannter LDAP Fehler')
+      expect(name_for_login('', ldap)).to eq('Fehler bei der LDAP-Suche.')
 
     end
   end
