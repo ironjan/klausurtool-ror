cce22b35c0e7f4027cea291fb3fc3615b7bf46fc
Jan Lippert <lippertsjan@gmail.com>
2016-05-29 22:35:50 +0200

    Documentation certification to 100%

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 # TODO is this Mailer needed?
-class ApplicationMailer < ActionMailer::Base
+class ApplicationMailer < ActionMailer::Base #:nodoc:
   default from: "noreply@ausleihe.fachschafts.website"
   layout 'mailer'
 end
