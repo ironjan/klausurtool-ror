a3650a86393df6da89cb5e44e1cb6e403f50c0c9
Jan Lippert <lippertsjan@gmail.com>
2016-06-08 22:38:04 +0200

    Extracted a complicated if-statement into a method

--- app/controllers/ausleihe_controller.rb
+++ app/controllers/ausleihe_controller.rb
@@ -27,35 +27,38 @@ class AusleiheController < ApplicationController
     folder_list = params[:folderList].split(/\r?\n/)
 
     Rails.logger.debug("Got switch request containing #{folder_list.count} elements.")
+
     folder_list = folder_list
                       .map { |i| [i, i.strip] }
                       .reject { |_, stripped| stripped.empty? }
                       .map { |f, stripped| append_instance_to_tuple(f, string_to_barcode_id(stripped)) }
 
-
-    corrected_codes = folder_list
-                          .select { |f, barcode_id, _| f != barcode_id && f.length != 4 && f.length != 8 }
-                          .map { |barcode, f| input_barcode_to_output_string(barcode, f) }
-
-
     lent_instances = folder_list.reject { |_, _, i| i.nil? || i.old_lend_out.nil? }
-
-
-    unless corrected_codes.empty?
-      flash[:warning] = "#{Time.new}: IDs wurden korrigiert: #{corrected_codes.join(', ')}"
-    end
 
     unless switch_request_is_valid(folder_list, lent_instances)
       redirect_to ausleihe_path and return
     end
 
+    corrected_codes = folder_list
+                          .select { |f, barcode_id, _| is_corrected_barcode?(barcode_id, f) }
+                          .map { |barcode, f| "#{barcode} (#{f})" }
+                          .join(', ')
+
+    unless corrected_codes.empty?
+      flash[:warning] = "#{Time.new}: IDs wurden korrigiert: #{corrected_codes}"
+    end
     redirect_to_after_switch_action(lent_instances.empty?, folder_list)
+  end
+
+  # true, if barcode_id is a fixed version of input string is
+  def is_corrected_barcode?(barcode_id, is)
+    is != barcode_id && is.length != 4 && is.length != 8
   end
 
   def switch_request_is_valid(folder_list, lent_instances)
     non_existing_instances = folder_list
                                  .select { |_, _, instance| instance.nil? }
-                                 .map { |barcode, f| input_barcode_to_output_string(barcode, f) }
+                                 .map { |barcode, f| "#{barcode} (#{f})" }
 
     has_non_existing_instances = (not non_existing_instances.empty?)
     if has_non_existing_instances
@@ -251,16 +254,13 @@ end
     [f, barcode_id, OldFolderInstance.find_by(barcodeId: barcode_id)]
   end
 
-  def input_barcode_to_output_string(barcode, f)
-    "#{barcode} (#{f})"
-  end
 
   def invalid_input_for_switch_message(folder_list, lent)
     lent_as_strings = lent
-                          .map { |barcode, f| input_barcode_to_output_string(barcode, f) }
+                          .map { |barcode, f| "#{barcode} (#{f})" }
                           .join(', ')
     all_as_strings = folder_list
-                         .map { |barcode, f| input_barcode_to_output_string(barcode, f) }
+                         .map { |barcode, f| "#{barcode} (#{f})" }
                          .join(', ')
 
     "#{Time.new}: Eingabe enthält gemischte Ordner. Entweder Ausleihen oder Zurücknehmen. Ordner-Exemplare: #{all_as_strings}, davon verliehen: #{lent_as_strings}"
