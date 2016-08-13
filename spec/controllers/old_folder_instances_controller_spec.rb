efadb0c4641814ad4a9a33802ebb7a70b027a7f0
Jan Lippert <lippertsjan@gmail.com>
2016-08-13 16:33:06 +0200

    Added test for the current behaviour of the destroy action

--- spec/controllers/old_folder_instances_controller_spec.rb
+++ spec/controllers/old_folder_instances_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 describe OldFolderInstancesController do
   it "reminds me to implement spec for this controller"
+
+  describe '#destroy' do
+
+    before(:each) do
+      folder = FactoryGirl.create(:old_folder, id: 123)
+    end
+
+    it 'deleting a non-existent folder should throw 404' do
+      expect {
+        process :destroy, method: :delete, params: {old_folder_id: 123, id: -1}
+      }.to raise_exception(ActiveRecord::RecordNotFound)
+    end
+  end
 end
