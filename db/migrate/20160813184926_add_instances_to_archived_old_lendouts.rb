e78802bb10b1ba00dad7b37fb8fd85eee601013b
Jan Lippert <lippertsjan@gmail.com>
2016-08-21 05:56:32 +0200

    Added missing nodoc

--- db/migrate/20160813184926_add_instances_to_archived_old_lendouts.rb
+++ db/migrate/20160813184926_add_instances_to_archived_old_lendouts.rb
@@ -1,4 +1,4 @@
-class AddInstancesToArchivedOldLendouts < ActiveRecord::Migration[5.0]
+class AddInstancesToArchivedOldLendouts < ActiveRecord::Migration[5.0] #:nodoc:
   def change
     change_column :archived_old_lend_outs, :old_folder_instances, :string
   end
