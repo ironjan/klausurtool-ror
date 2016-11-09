8024504717bc0fdec43018440a667f60b5817c2f
Jan Lippert <lippertsjan@gmail.com>
2016-11-09 20:10:49 +0100

    Added spec for ReminderMailsHelper

--- spec/helpers/reminder_mails_helper_spec.rb
+++ spec/helpers/reminder_mails_helper_spec.rb
@@ -3,6 +3,26 @@ require 'rails_helper'
 describe ReminderMailsHelper do
 
   describe 'recipient' do
-    it 'should remind me to add specs'
+    it 'return the imt-name with appended mail suffix' do
+      expect(recipient(FactoryGirl.build(:old_lend_out, imt: 'nil'))).to eq('nil@mail.upb.de')
+      expect(recipient(FactoryGirl.build(:old_lend_out, imt: 'abc'))).to eq('abc@mail.upb.de')
+      expect(recipient(FactoryGirl.build(:old_lend_out, imt: 'ljan'))).to eq('ljan@mail.upb.de')
+    end
+  end
+
+  describe 'subject' do
+  	it 'returns the correct subject' do
+  	  expect(subject()).to eq('[Klausurausleihe] Verliehene Ordner/Mappen zeitnah zurückbringen')
+  	end
+  end
+
+  describe 'cc' do
+  	it 'reminds me to spec' do
+  	  expect(cc()).to eq('fsmi@upb.de; fsmi-klausurarchiv@lists.upb.de')
+    end
+  end
+
+  describe 'body' do
+  	it 'reminds me to spec'
   end
 end
